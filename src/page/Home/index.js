b9713fdc91850352a596a254a586193a3174ff65
xiaoqiang <642848150@qq.com>
2020-04-19 19:11:13 +0800

    初始化

---
+++ src/page/Home/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import {Route , link} from 'react-router-dom'
+class home extends Component {
+    render() {
+        return (
+            <div className="home">
+                <Route />
+            </div>
+        )
+    }
+}
---
+++ src/page/NotFound/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+const NotFound = function () {
+    return (
+        <div>
+            <h1>页面不存在......404!</h1>
+        </div>
+    )
+}
+export default NotFound;
